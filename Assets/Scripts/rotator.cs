fd64889f657f525a773f1efc6b289119271949a2
Andrew Crisp <andrewcrisp556@hotmail.com>
2015-07-23 20:57:49 -0600

    Initial commit

---
+++ Assets/Scripts/rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 25, 46) * Time.deltaTime);
+	}
+}
